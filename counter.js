059bd0c1263c0d73a261b59047a74353c3d196f6
boffskbob <acornbear03@gmail.com>
2024-06-27 16:05:12 -0700

    create main.js, delete other js file

--- counter.js
+++
@@ -1,9 +0,0 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => document.getElementById("img").src = "https://images.dog.ceo/breeds/kombai/Kombai-indian-Dog.jpg")
-  setCounter(0)
-}
