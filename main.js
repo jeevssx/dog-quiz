40b6b571581330891f7904b1f3277996457683a9
boffskbob <acornbear03@gmail.com>
2024-06-28 03:18:15 -0400

    revised fetch command, changed style layout
    Changed fetch command to async await style, added
    4 images of dogs on front page and styled using
    flex. Added gallery wrapper class around all
    images. Changed way to identify image from ID to
    class array (can be reverted).

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(data => {
-        const dataContainer = document.getElementById('data-container');
-        const img = document.createElement('img');
-        img.src = data.message;
-        dataContainer.innerHTML = '';
-        dataContainer.appendChild(img);
-    })
+// fetch('https://dog.ceo/api/breeds/image/random')
+//     .then(response => response.json())
+//     .then(data => {
+//         const dataContainer = document.getElementsByClassName('incomplete')[0];
+//         const img = document.createElement('img');
+//         img.src = data.message;
+//         dataContainer.className = 'complete';
+//         dataContainer.innerHTML = '';
+//         dataContainer.appendChild(img);
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         const dataContainer = document.getElementsByClassName('incomplete')[0];
+//         const img = document.createElement('img');
+//         img.src = data.message;
+//         dataContainer.className = 'complete';
+//         dataContainer.innerHTML = '';
+//         dataContainer.appendChild(img);
+//     })
+
+async function getImage() {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await response.json();
+    return data;
+}
+for (i = 0; i < 4; i++) {
+    getImage()
+        .then(data => {
+            const dataContainer = document.getElementsByClassName('incomplete')[0];
+            const img = document.createElement('img');
+            img.src = data.message;
+            img.className = 'image';
+            dataContainer.className = 'complete';
+            dataContainer.innerHTML = '';
+            dataContainer.appendChild(img);
+        })
+}
