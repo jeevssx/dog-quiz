7ccd4ed2d3ea33f2b404f522938c1bf8c9f50138
jeevssx <1jeevanshahi@gmail.com>
2024-06-28 12:14:19 -0400

    add button to enter game

--- main.js
+++ main.js
@@ -1,28 +1,16 @@
-// fetch('https://dog.ceo/api/breeds/image/random')
-//     .then(response => response.json())
-//     .then(data => {
-//         const dataContainer = document.getElementsByClassName('incomplete')[0];
-//         const img = document.createElement('img');
-//         img.src = data.message;
-//         dataContainer.className = 'complete';
-//         dataContainer.innerHTML = '';
-//         dataContainer.appendChild(img);
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         const dataContainer = document.getElementsByClassName('incomplete')[0];
-//         const img = document.createElement('img');
-//         img.src = data.message;
-//         dataContainer.className = 'complete';
-//         dataContainer.innerHTML = '';
-//         dataContainer.appendChild(img);
-//     })
-
+/**
+ * gets JSON Obj
+ * @returns a JSON object with message for link for image as wel as status of the request
+ */
 async function getImage() {
     const response = await fetch('https://dog.ceo/api/breeds/image/random');
     const data = await response.json();
     return data;
+    
 }
+/**
+ * loop 4 times for images
+ */
 for (i = 0; i < 4; i++) {
     getImage()
         .then(data => {
